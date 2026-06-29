18cb04d43f7687bf07bd0d7e340d2c799706f3da
Kitty Knight <kittyknight02@gmail.com>
2026-06-29 16:45:16 +0100

    Update source

--- view_data.py
+++ view_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-rosello2015 = pd.read_csv("rosello2015_supplementary1.csv")
+rosello2015 = pd.read_csv("Data/rosello2015_supplementary1.csv")
 # print(rosello2015.info())
 print(rosello2015["Outbreak"].value_counts())
 print(rosello2015["Year_outbreak"].value_counts())
