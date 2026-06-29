56fb5f8c065fcd3084a8ab74d697b9ea5dad1e4d
Kitty Knight <kittyknight02@gmail.com>
2026-06-29 17:08:02 +0100

    Added data summaries

--- view_data.py
+++ view_data.py
@@ -4,3 +4,12 @@ rosello2015 = pd.read_csv("Data/rosello2015_supplementary1.csv")
 # print(rosello2015.info())
 print(rosello2015["Outbreak"].value_counts())
 print(rosello2015["Year_outbreak"].value_counts())
+
+sierraleone2014 = pd.read_csv("Data/ebola_sierraleone_2014_confirmed.csv")
+print(sierraleone2014.info())
+
+uganda2022 = pd.read_csv("Data/Uganda2022globaldothealth.csv")
+print(uganda2022.info())
+
+DRC2018 = pd.read_csv("Data/DRC2018_humdata_MOH-Total.csv")
+print(DRC2018.info())
